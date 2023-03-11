bec774905bbee906b93d05fcfd40fe47551214df
Andrey <andrey.ra4kov1988@gmail.com>
2023-03-11 14:11:50 +0300

    T66

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int M = ReadInt("enter number M: ");
+int N = ReadInt("enter number N: ");
+if (M < N) 
+{
+    int temp = M;
+    M = N;
+    N = temp;
+}
+  System.Console.WriteLine(Print(M, N));
+
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Print(int numN, int numM)
+{
+    int result = numN;
+    if (numN > numM)
+    {
+        return result + Print(numN-1, numM);
+    }
+    return result;
+}
